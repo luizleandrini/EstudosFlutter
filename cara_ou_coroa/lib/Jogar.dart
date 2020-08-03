a741e789b41a666da5a19a3d2be57b07972fa991
LuizLeandrini <guilhermeleandrini@hotmail.com>
2020-08-03 18:32:35 -0300

    Cara ou Coroa

---
+++ cara_ou_coroa/lib/Jogar.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+
+import 'package:cara_ou_coroa/Resultado.dart';
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado(){
+
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt(itens.length);
+    var resultado = itens[numero];
+
+    Navigator.push(  // definir rota ou tela que queira abrir
+      context, 
+      MaterialPageRoute(                         
+        builder: (context) => Resultado("Cara"),
+      )
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      //backgroundColor: Color.fromRGBO(255, 204, 128, 1),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset("imagens/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("imagens/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
